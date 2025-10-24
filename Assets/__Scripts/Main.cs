a0052457abb1b3430d48b62e024b0f9c404489e2
jadabrown822 <jadabrown822@gmail.com>
2025-10-24 18:52:27 -0500

    Spawning Enemies at Random

---
+++ Assets/__Scripts/Main.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;      // Enables the loading and reloading of scenes
+
+public class Main : MonoBehaviour
+{
+    static private Main S;      // A private Singleton for Main
+
+    [Header("Inscribed")]
+    public GameObject[] prefabEnemies;              // Array of Enmey prefabs
+    public float enemySpawnPerSecond = 0.5f;        //  # enemeis spawned/second
+    public float enemyInsetDefault = 1.5f;           // Inset from the sides
+
+    private BoundsCheck bndCheck;
+
+
+    private void Awake()
+    {
+        S = this;
+
+        // Set bndCheck to reference the BoundsCheck component on this GameObject
+        bndCheck = GetComponent<BoundsCheck>();
+
+        // Invoke SpawnEnemy() once (in 2 seconds, based on default values)
+        Invoke(nameof(SpawnEnemy), 1f / enemySpawnPerSecond);
+    }
+
+
+    public void SpawnEnemy()
+    {
+        // Pick a random Enemy prefab to instantiate
+        int ndx = Random.Range(0, prefabEnemies.Length);
+        GameObject go = Instantiate<GameObject>(prefabEnemies[ndx]);
+
+        // Position the Enemy above the screen with a random x position
+        float enemyInset = enemyInsetDefault;
+        if (go.GetComponent<BoundsCheck>() != null)
+        {
+            enemyInset = Mathf.Abs(go.GetComponent<BoundsCheck>().radius);
+        }
+
+        // Set the initial position for the SpawnEnemy
+        Vector3 pos = Vector3.zero;
+        float xMin = -bndCheck.camWidth + enemyInset;
+        float xMax = bndCheck.camWidth - enemyInset;
+        pos.x = Random.Range(xMin, xMax);
+        pos.y = bndCheck.camHeight + enemyInset;
+        go.transform.position = pos;
+
+        // Invoke SpawnEnemy()
+        Invoke(nameof(SpawnEnemy), 1f / enemySpawnPerSecond);
+    }
+
+
+    /*
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    */
+}
