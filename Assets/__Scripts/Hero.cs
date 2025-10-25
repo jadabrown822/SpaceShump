a297aea2c67b8c091b1e92b7d59a08d21ba83322
jadabrown822 <jadabrown822@gmail.com>
2025-10-24 19:18:45 -0500

    Making the Enemies Damage the Player

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -12,8 +12,13 @@ public class Hero : MonoBehaviour
     public float rollMult = -45;
     public float pitchMult = 30;
 
-    [Header("Dynamic")]     [Range(0,4)]
-    public float shieldLevel = 1;
+    [Header("Dynamic")]
+    [Range(0, 4)]
+    [SerializeField]
+    private float _shieldLevel = 1;
+    // public float shieldLevel = 1;
+    [Tooltip("This filed holds a reference to the last triggering GameObject")]
+    private GameObject lastTriggerGo = null;
 
 
     void Awake()
@@ -44,6 +49,45 @@ public class Hero : MonoBehaviour
 
         // Rotate the ship to make it feel more dynamic
         transform.rotation = Quaternion.Euler(vAxis * pitchMult, hAxis * rollMult, 0);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Transform rootT = other.gameObject.transform.root;
+        GameObject go = rootT.gameObject;
+        // Debug.Log("Shield trigger hit by: " + go.gameObject.name);
+
+        // Make sure it's not the same trigger go as last time
+        if (go == lastTriggerGo) { return; }
+        lastTriggerGo = go;
+
+        Enemy enemy = go.GetComponent<Enemy>();
+        if (enemy != null)
+        {       // If teh shield was triggered by an enemy
+            shieldLevel--;      // Decreases the level of the shield by 1
+            Destroy(go);        // ... and Destroy the enemy
+        }
+        else
+        {
+            Debug.LogWarning("Shield trigger hit by non-enemy: " + go.name);
+        }
+    }
+
+
+    public float shieldLevel
+    {
+        get { return (_shieldLevel); }
+        private set
+        {
+            _shieldLevel = Mathf.Min(value, 4);
+
+            // If the shiled is going to be set to less than zero
+            if (value < 0)
+            {
+                Destroy(this.gameObject);       // Destroy the Hero
+            }
+        }
     }
 
 
