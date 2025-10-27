18a358855581bb140ee9a07fa6aac1b91d7fd9d2
jadabrown822 <jadabrown822@gmail.com>
2025-10-27 15:32:35 -0500

    Modifying the ProjectileHero Class to Use WeaponDefinitions

--- Assets/__Scripts/ProjectileHero.cs
+++ Assets/__Scripts/ProjectileHero.cs
@@ -7,11 +7,25 @@ using UnityEngine;
 public class ProjectileHero : MonoBehaviour
 {
     private BoundsCheck bndCheck;
+    private Renderer rend;
+
+    [Header("Dynamic")]
+    public Rigidbody rigid;
+    [SerializeField] private eWeaponType _type;
+
+    // Thispublic property masks the private field _type
+    public eWeaponType type
+    {
+        get { return (_type); }
+        set { SetType(value); }
+    }
 
 
     private void Awake()
     {
         bndCheck = GetComponent<BoundsCheck>();
+        rend = GetComponent<Renderer>();
+        rigid = GetComponent<Rigidbody>();
     }
 
 
@@ -22,6 +36,27 @@ public class ProjectileHero : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+
+    /*
+        Set the _type private field and colors thos projectile to match the WeaponDefinition
+    
+        <param name="eType">The eWeaponType to use</param>
+    */
+    public void SetType(eWeaponType eType)
+    {
+        _type = eType;
+        WeaponDefinition def = Main.GET_WEAPON_DEFINITION(_type);
+        rend.material.color = def.projectileColor;
+    }
+
+
+    // Allows Weapon to easily set the velocity of this ProjectHero
+    public Vector3 vel
+    {
+        get { return rigid.velocity; }
+        set { rigid.velocity = value; }
     }
     
 
