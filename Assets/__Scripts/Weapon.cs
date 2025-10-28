f01e29ae8538d677b4d18e357c822a6026c9bcb1
jadabrown822 <jadabrown822@gmail.com>
2025-10-28 12:40:59 -0500

    The Serializable WeaponDefinition Class

--- Assets/__Scripts/Weapon.cs
+++ Assets/__Scripts/Weapon.cs
@@ -18,6 +18,44 @@ public enum eWeaponType
     shield          // Raise shieldLevel
 }
 
+/*
+    The WeaponDefinition class allow to set the properties
+        of a specific weapon in the Inspector. The Main class has
+        an array of WeaponDefinitions that make this possible
+*/
+[System.Serializable]
+public class WeaponDefinition
+{
+    public eWeaponType type = eWeaponType.none;
+
+    [Tooltip("Letter to show on the PowerUp Cube")]
+    public string letter;
+
+    [Tooltip("Color of PowerUp Cube")]
+    public Color powerUpColor = Color.white;
+
+    [Tooltip("Prefab of Weapon model that is attached to the Player Ship")]
+    public GameObject weaponModelPrefab;
+
+    [Tooltip("Prefab of projectile that is fired")]
+    public GameObject projectilePrefab;
+
+    [Tooltip("Color of the projectile that is fired")]
+    public Color projectileColor = Color.white;
+
+    [Tooltip("Damage caused when a sinlge Projectile hits an Enemy")]
+    public float damageOnHit = 0;
+
+    [Tooltip("Damage caused per second by the Laser [Not Implemented]")]
+    public float damagePerSec = 0;
+
+    [Tooltip("Seconds to delay bewteen shots")]
+    public float delayBetweenShots = 0;
+
+    [Tooltip("Velocity of individual Projectiles")]
+    public float velocity = 50;
+}
+
 public class Weapon : MonoBehaviour
 {
     // Start is called before the first frame update
