635daea514d6287e2d2d67186e3ebe94defd6244
jadabrown822 <jadabrown822@gmail.com>
2025-10-27 21:49:45 -0500

    fix the shooting

--- Assets/__Scripts/Main.cs
+++ Assets/__Scripts/Main.cs
@@ -1,131 +1,105 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;      // Enables the loading and reloading of scenes
-
-public class Main : MonoBehaviour
-{
-    static private Main S;      // A private Singleton for Main
-    static private Dictionary<eWeaponType, WeaponDefinition> WEAP_DICT;
-
+using UnityEngine.SceneManagement;    // Enables the loading & reloading of scenes
+  
+public class Main : MonoBehaviour {
+    static private Main S;    // A private singleton for Main
+    // New static field to hold the Weapon Definitions dictionary
+    static public Dictionary<eWeaponType, WeaponDefinition> WEAP_DICT;
+  
     [Header("Inscribed")]
     public bool spawnEnemies = true;
-    public GameObject[] prefabEnemies;              // Array of Enmey prefabs
-    public float enemySpawnPerSecond = 0.5f;        //  # enemeis spawned/second
-    public float enemyInsetDefault = 1.5f;           // Inset from the sides
+    public GameObject[] prefabEnemies;          // Array of Enemy prefabs
+    public float enemySpawnPerSecond = 0.5f;    // # Enemies spawned/second
+    public float enemyInsetDefault = 1.5f;      // Inset from the sides
     public float gameRestartDelay = 2;
+    // Array of WeaponDefinition objects, set in the Inspector
     public WeaponDefinition[] weaponDefinitions;
-
+  
     private BoundsCheck bndCheck;
-
-
-    private void Awake()
-    {
+  
+    void Awake() {
         S = this;
-
+  
         // Set bndCheck to reference the BoundsCheck component on this GameObject
         bndCheck = GetComponent<BoundsCheck>();
-
+  
         // Invoke SpawnEnemy() once (in 2 seconds, based on default values)
-        Invoke(nameof(SpawnEnemy), 1f / enemySpawnPerSecond);
+        Invoke(nameof(SpawnEnemy), 1f/enemySpawnPerSecond);
 
         // A generic Dictionary with eWeaponType as the key
         WEAP_DICT = new Dictionary<eWeaponType, WeaponDefinition>();
-        foreach (WeaponDefinition def in weaponDefinitions)
-        {
+        
+        // Loop through all WeaponDefinitions and add them to the Dictionary
+        foreach (WeaponDefinition def in weaponDefinitions) {
             WEAP_DICT[def.type] = def;
         }
     }
-
-
-    public void SpawnEnemy()
-    {
-        // If spawnEnemies is false, skip to the next invoke of SpawnEnemy()
-        if (!spawnEnemies) 
-        {
+  
+  
+    public void SpawnEnemy() {
+        // If spawnEnemies is false, skip to the next invoke of SpawEnemy()
+        if (!spawnEnemies) {
             Invoke(nameof(SpawnEnemy), 1f / enemySpawnPerSecond);
             return;
         }
-    
+
         // Pick a random Enemy prefab to instantiate
         int ndx = Random.Range(0, prefabEnemies.Length);
         GameObject go = Instantiate<GameObject>(prefabEnemies[ndx]);
-
-        // Position the Enemy above the screen with a random x position
+  
+        // position the Enemy above the screen with a random x position
         float enemyInset = enemyInsetDefault;
-        if (go.GetComponent<BoundsCheck>() != null)
-        {
+        if (go.GetComponent<BoundsCheck>() != null) {
             enemyInset = Mathf.Abs(go.GetComponent<BoundsCheck>().radius);
         }
-
-        // Set the initial position for the SpawnEnemy
+  
+        // Set the initial position for the spawned Enemy
         Vector3 pos = Vector3.zero;
         float xMin = -bndCheck.camWidth + enemyInset;
         float xMax = bndCheck.camWidth - enemyInset;
         pos.x = Random.Range(xMin, xMax);
         pos.y = bndCheck.camHeight + enemyInset;
         go.transform.position = pos;
-
-        // Invoke SpawnEnemy()
-        Invoke(nameof(SpawnEnemy), 1f / enemySpawnPerSecond);
+  
+        // Invoke SpawnEnemy() again
+        Invoke(nameof(SpawnEnemy), 1f/enemySpawnPerSecond);
     }
 
 
-    void DelayRestart()
-    {
+    void DelayedRestart() { // Changed to DelayedRestart to match the call in HERO_DIED
         // Invoke the Restart() method in gameRestartDelay seconds
         Invoke(nameof(Restart), gameRestartDelay);
     }
 
 
-    void Restart()
-    {
+    void Restart() {
         SceneManager.LoadScene("__Scene_0");
     }
 
 
-    static public void HERO_DIED()
-    {
-        S.DelayRestart();
+    static public void HERO_DIED() {
+        S.DelayedRestart();
     }
-
-
-    /*
-        Static function that gets a WeaponDefinition from the WEAP_DICT static
-            protected field of the Main class
-
-        <returns>The WeaponDefinition, of it there is no WeaponDefinition with
-                    the eWeaponType passes in, returns a new WeaponDefinition with a
-                    eWeaponType of eWeaponType.none</returns>
-       
-        <param name="wt">The eWeaponType of the desored WeaponDefinition</param>
+  
+    /* Static function that gets a WeaponDefinition from the WEAP_DICT static
+              protected field of the Main class
+              <returns>The WeaponDefinition, or if there is no WeaponDefinition with
+              the eWeaponType passed in, returns a new WeaponDefinition with a
+              eWeaponType of eWeaponType.none</returns>
+              <param name = "wt">The eWeaponType of the desired WeaponDefinition</param>    
     */
-    static public WeaponDefinition GET_WEAPON_DEFINITION(eWeaponType wt)
-    {
-        if (WEAP_DICT.ContainsKey(wt))
-        {
-            return (WEAP_DICT[wt]);
+    static public WeaponDefinition GET_WEAPON_DEFINITION(eWeaponType wt) {
+        // FIX: Corrected typo 'ContainKey' to 'ContainsKey' and correct return value
+        if (WEAP_DICT.ContainsKey(wt)) {
+            return(WEAP_DICT[wt]);
         }
 
         /*
-            If no entry of the correct type exists to WEAP_DICT, return a new 
-                WeaponDefinition with a type of eWeaponType.none (the default value)
+            If no entry of the correct type exists in WEAP_DICT, return a new
+            WeaponDefinition with a type of eWeaponType.none (the default value)
         */
-        return (new WeaponDefinition());
+        return(new WeaponDefinition());
     }
-
-
-    /*
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
-    */
 }
--- Assets/__Scripts/Weapon.cs
+++ Assets/__Scripts/Weapon.cs
@@ -2,158 +2,160 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/*
- * This is an enum of varius possible weapon types
- * It also includes a "shield" type to allow a shield PowerUp
- * Items marked [NI] below are Not Implemented
+/* This is an enum of the various possible weapon types
+    It also includes a "shield" type to allow a shield PowerUp
+    Items marked [NI] below are Not Implemented  
 */
-
-public enum eWeaponType
-{
-    none,           // The default / no weapon
-    blaster,        // A simlple blaster
-    spread,         // Multiple shots simultaneously
-    phaser,         // [NI] Shots that move in waves
-    missile,        // [NI] Homing missiles
-    laser,          // [NI] Damage over time
-    shield          // Raise shieldLevel
+public enum eWeaponType {
+    none,        // The default / no weapon
+    blaster,    // A simple blaster
+    spread,      // Multiple shots simultaneously
+    phaser,      // [NI] Shots that move in waves
+    missile,    // [NI] Homing missiles
+    laser,      // [NI] Damage over time
+    shield      // Raise shieldLevel
 }
 
-/*
-    The WeaponDefinition class allows to set the properties
-        of a specific weapon in the Inspector. The Main class has
-        an array of WeaponDefinitions that make this possible
+/* The WeaponDefinition class allows to set the properties
+      of a specific weapon in the Inspector. The Main class has
+      an array of WeaponDefinitions that make this possible  
 */
 [System.Serializable]
-public class WeaponDefinition 
-{
-    public eWeaponType type = eWeaponType.none;
+public class WeaponDefinition {
+    public eWeaponType    type = eWeaponType.none;
 
     [Tooltip("Letter to show on the PowerUp Cube")]
-    public string letter;
+    public string    letter;
 
     [Tooltip("Color of PowerUp Cube")]
-    public Color powerUpColor = Color.white;
+    public Color    powerUpColor = Color.white;
 
     [Tooltip("Prefab of Weapon model that is attached to the Player Ship")]
-    public GameObject weaponModelPrefab;
+    public GameObject    weaponModelPrefab;
 
     [Tooltip("Prefab of the Projectile that is fired")]
-    public GameObject projectilePrefab;
+    public GameObject    projectilePrefab;
 
     [Tooltip("Color of the Projectile that is fired")]
-    public Color projectileColor = Color.white;
+    public Color    projectileColor = Color.white;
 
     [Tooltip("Damage caused when a single Projectile hits an Enemy")]
-    public float damageOnHit = 0;
+    public float    damageOnHit = 0;
 
-    [Tooltip("Damage caused per second by a Laser [Not Implemented")]
-    public float damagePerSec = 0;
+    [Tooltip("Damage caused per second by a Laser [Not Implemented]")]
+    public float    damagePerSec = 0;
 
     [Tooltip("Seconds to delay between shots")]
-    public float delayBetweenShots = 0;
+    public float     delayBetweenShots = 0; // Corrected typo: delayedBetweenShots -> delayBetweenShots
 
-    [Tooltip("Velocity of individual Projectils")]
-    public float velocity = 50;
+    [Tooltip("Velocity of individual Projectiles")]
+    public float    velocity = 50;
 }
 
-public class Weapon : MonoBehaviour
-{
+public class Weapon : MonoBehaviour {
+    // Static Transform used to hold all ProjectileGameObjects in the Hierarchy
     static public Transform PROJECTILE_ANCHOR;
 
     [Header("Dynamic")]
     [SerializeField]
-    [Tooltip("Setting this manually while playing does not properly")]
-    private eWeaponType _type = eWeaponType.none;
+    [Tooltip("Setting this manually while playing does not work properly")]
+    private eWeaponType     _type = eWeaponType.none;
     public WeaponDefinition def;
-    public float nextShotTime;      // Time the Weapon will fire next
+    public float        nextShotTime;       // Time the Weapon will fire next
 
-    private GameObject weaponModel;
-    private Transform shotPointTrans;
+    private GameObject      weaponModel;
+    private Transform       shotPointTrans;
+
+    // Public property to access the private _type field and call SetType on assignment
+    public eWeaponType type {
+        get { return _type; }
+        set { SetType(value); } 
+    }
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        // Set the PROJECTILE_ANCHOR if it has not already been done
-        if (PROJECTILE_ANCHOR == null)
-        {
+    void Start() {
+        // Set up PROJECTILE_ANCHOR if it has not already been done
+        if (PROJECTILE_ANCHOR == null) {
             GameObject go = new GameObject("_ProjectileAnchor");
             PROJECTILE_ANCHOR = go.transform;
         }
 
+        // ShotPoint is assumed to be the first child (index 0) of the Weapon GameObject
         shotPointTrans = transform.GetChild(0);
 
-        // Call Settype() for the default _type set in the Inspector
+        // Call SetType() for the default _type set in the Inspector
         SetType(_type);
 
-        // Find the fireEvent of a Hero Component in the parent hierarchy
+        // Find the Hero component in the parent hierarchy
         Hero hero = GetComponentInParent<Hero>();
-        if (hero != null)
-        {
+        if (hero != null) {
+            // Subscribe the Fire method to the Hero's fireEvent delegate
             hero.fireEvent += Fire;
         }
     }
 
 
-    public void SetType(eWeaponType wt)
-    {
+    public void SetType(eWeaponType wt) {
         _type = wt;
-        if (_type == eWeaponType.none)
-        {
+        if (type == eWeaponType.none) {
             this.gameObject.SetActive(false);
             return;
         }
-        else
-        {
+        else {
             this.gameObject.SetActive(true);
         }
 
-        // Get the WeaponDefinition fo this type from Main
+        // Get the WeaponDefinition for this type from Main
         def = Main.GET_WEAPON_DEFINITION(_type);
 
         // Destroy any old model and then attach a model for this weapon
-        if (weaponModel != null)
-        {
+        if (weaponModel != null) {
             Destroy(weaponModel);
         }
-        weaponModel = Instantiate<GameObject>(def.weaponModelPrefab, transform);
-        weaponModel.transform.localPosition = Vector3.zero;
-        weaponModel.transform.localScale = Vector3.one;
+        
+        if (def.weaponModelPrefab != null) {
+            weaponModel = Instantiate<GameObject>(def.weaponModelPrefab, transform);
+            weaponModel.transform.localPosition = Vector3.zero;
+            weaponModel.transform.localScale = Vector3.one;
+        }
 
         nextShotTime = 0;       // Can fire immediately after _type is set
     }
 
 
-    private void Fire()
-    {
-        // Of this.gameObject is inactive, return
-        if (!gameObject.activeInHierarchy)
-        {
+    private void Fire() {
+        // If this.gameObject is inactive, return
+        if (!gameObject.activeInHierarchy) {
             return;
         }
         // If its hasn't been enough time between shots, return
-        if (Time.time < nextShotTime)
-        {
+        if (Time.time < nextShotTime) {
             return;
         }
 
         ProjectileHero p;
         Vector3 vel = Vector3.up * def.velocity;
 
-        switch(_type)
-        {
+        // Set the time for the next shot based on the delay
+        nextShotTime = Time.time + def.delayBetweenShots;
+
+        switch(type) {
             case eWeaponType.blaster:
                 p = MakeProjectile();
                 p.vel = vel;
                 break;
 
             case eWeaponType.spread:
+                // Middle shot
                 p = MakeProjectile();
                 p.vel = vel;
+                
+                // Left shot
                 p = MakeProjectile();
                 p.transform.rotation = Quaternion.AngleAxis(10, Vector3.back);
                 p.vel = p.transform.rotation * vel;
+                
+                // Right shot
                 p = MakeProjectile();
                 p.transform.rotation = Quaternion.AngleAxis(-10, Vector3.back);
                 p.vel = p.transform.rotation * vel;
@@ -162,27 +164,21 @@ public class Weapon : MonoBehaviour
     }
 
 
-    private ProjectileHero MakeProjectile()
-    {
+    private ProjectileHero MakeProjectile() {
         GameObject go;
+        // CRITICAL FIX: The Instantiate call was incorrect. We need to instantiate the projectilePrefab
+        // and set the PROJECTILE_ANCHOR as its parent.
         go = Instantiate<GameObject>(def.projectilePrefab, PROJECTILE_ANCHOR);
+        
         ProjectileHero p = go.GetComponent<ProjectileHero>();
 
         Vector3 pos = shotPointTrans.position;
         pos.z = 0;
         p.transform.position = pos;
 
-        p.type = _type;
-        nextShotTime = Time.time + def.delayBetweenShots;
-        return (p);
-    }
-
-
-    /*
-        // Update is called once per frame
-        void Update()
-        {
+        // Set the projectile type, which also handles coloring and setting damage
+        p.type = type; 
         
-        }
-    */
+        return(p);
+    }
 }
