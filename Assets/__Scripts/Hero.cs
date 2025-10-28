6ab38bedb18a31afede8a7f73d7689ed16c55cea
jadabrown822 <jadabrown822@gmail.com>
2025-10-28 18:48:00 -0500

    Race Conditions & Script Execution Order

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -38,6 +38,10 @@ public class Hero : MonoBehaviour
         }
 
         // fireEvent += TempFire;
+
+        // Reset the weapons to start _Hero with 1 blaster
+        ClearWeapons();
+        weapons[0].SetType(eWeaponType.blaster);
     }
 
 
